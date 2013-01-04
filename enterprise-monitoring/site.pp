ed00c6b2796ba2452453b74b973d2fcf9930c50b
Nick Satterly <nick.satterly@guardian.co.uk>
2013-01-04 16:45:55 +0000

    Update monitoring to include alerta

--- enterprise-monitoring/site.pp
+++ enterprise-monitoring/site.pp
@@ -19,16 +19,26 @@ class yum {
 		priority        => 4,
 		metadata_expire => 300
 	}
+
+	yumrepo { '10gen':
+		descr           => '10gen Repository',
+		baseurl         => "${repo}/10gen/redhat/os/\$basearch",
+		enabled         => 1,
+		gpgcheck        => 0,
+		priority        => 4,
+		metadata_expire => 300
+	}
 }
 
 class monitoring {
 
-	class { 'fitb':
-		mysql_root_password => '4tyWvnxRaa07nUVj1b3Q',
-		fitb_db_password    => 'nguzpQwDYF8nZqpnXwo4',
-		config_include_glob => '/etc/fitb/conf.d/snmp*.php'
-	}
+	# class { 'fitb':
+		# mysql_root_password => '4tyWvnxRaa07nUVj1b3Q',
+		# fitb_db_password    => 'nguzpQwDYF8nZqpnXwo4',
+		# config_include_glob => '/etc/fitb/conf.d/snmp*.php'
+	# }
 
+	include alerta
 }
 
 node default {
