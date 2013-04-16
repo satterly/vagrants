a3efb312b10f48f0bdc0fe25bccf833319d90b0e
Nick Satterly <nick.satterly@guardian.co.uk>
2013-04-16 16:52:34 +0100

    update debian-build setup.sh

--- debian-build/setup.sh
+++ debian-build/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get -y install autoconf automake libtool ncompress make gcc git libexpat1-dev libapr1-dev libconfuse-dev libpcre3-dev rrdtool gperf
+apt-get -y install autoconf automake libtool ncompress make gcc git libexpat1-dev libapr1-dev libconfuse-dev libpcre3-dev rrdtool-dev gperf
 
 mkdir ~/build
 git clone git://git.code.sf.net/p/git2dist/code ~/build/git2dist
