afde1c2842efb54c4cbf48c5fc48ef4e1a23bcd1
Nick Satterly <nick.satterly@guardian.co.uk>
2014-02-24 22:02:53 +0000

    automate ck install

--- ganglia-dev/scripts/centos.sh
+++ ganglia-dev/scripts/centos.sh
@@ -1,18 +1,27 @@
-#!/bin/sh -e
-
-#set -x
+#!/bin/sh
 
 echo "Install build system for Ganglia with Riemann support"
 
-yum install autoconf automake libtool
-yum install apr-devel libconfuse-devel rrdtool-devel expat-devel pcre-devel
-yum install protobuf-compiler protobuf-c-devel
+yum install -y git curl wget autoconf automake libtool
+yum install -y apr-devel libconfuse-devel rrdtool-devel expat-devel pcre-devel
 
-wget http://concurrencykit.org/releases/ck-0.3.5.tar.gz
-tar zxvf ck*.tar.gz
-cd ck-0.3.5 && ./configure && make && sudo make install
+wget http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+rpm -Uvh epel-release-6-8.noarch.rpm
+yum install -y protobuf-compiler protobuf-c-devel
 
-yum install java-1.7.0-openjdk
+cd ~
+yum install -y rpmdevtools
+rpmdev-setuptree
+pushd rpmbuild/SOURCES
+wget http://concurrencykit.org/releases/ck-0.4.tar.gz
+popd
+pushd rpmbuild/SPECS
+wget http://concurrencykit.org/releases/ck-0.4.spec
+rpmbuild -bb ck-0.4.spec
+popd
+rpm -Uvh rpmbuild/RPMS/x86_64/ck*-0.4-1.el6.x86_64.rpm
+
+yum install -y java-1.7.0-openjdk
 wget -q http://aphyr.com/riemann/riemann-0.2.4.tar.bz2
 tar xvfj riemann-*.tar.bz2
 cd riemann-*
