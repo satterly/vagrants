6d7da90ce106136e54bc1ed0ccd1a0eec03c29ce
Nick Satterly <nick.satterly@guardian.co.uk>
2014-03-29 12:13:10 +0000

    update ganglia-dev setup script

--- ganglia-dev/scripts/debian.sh
+++ ganglia-dev/scripts/debian.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-#set -x
+set -x
 
 echo "Install build system for Ganglia with Riemann support"
 
@@ -13,15 +13,26 @@ apt-get -y install libcurl4-openssl-dev
 
 # apt-get -y install python-stdeb devscripts
 
-wget http://concurrencykit.org/releases/ck-0.3.5.tar.gz
+test ! -f ck-0.3.5.tar.gz && wget http://concurrencykit.org/releases/ck-0.3.5.tar.gz
 tar zxvf ck*.tar.gz
-cd ck-0.3.5 && ./configure && make && sudo make install
+test ! -d ck-0.3.5 && pushd ck-0.3.5 && ./configure && make && sudo make install && popd
 
 apt-get -y install openjdk-7-jre
-wget -q http://aphyr.com/riemann/riemann-0.2.4.tar.bz2
-tar xvfj riemann-*.tar.bz2
-cd riemann-*
-mkdir /var/log/riemann
+test ! -f riemann-0.2.4.tar.bz2 && wget -q http://aphyr.com/riemann/riemann-0.2.4.tar.bz2
+tar xvfj riemann-0.2.4.tar.bz2
+test ! -d /var/log/riemann && mkdir /var/log/riemann
+echo `pwd`
+pushd riemann-0.2.4
 nohup bin/riemann etc/riemann.config 2>&1 &
+popd
+
+git clone -b release/3.7 https://github.com/ganglia/monitor-core.git ganglia-3.7.0
+pushd ganglia-3.7.0
+./bootstrap
+./configure --with-gmetad --enable-perl \
+            --enable-php --enable-status --with-python \
+            --with-riemann
+make dist
+popd
 
 echo "Done!"
